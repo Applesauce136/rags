8e3a4b6eec2f962b818fb51f9e60e4e7079f3f89
Applesauce136 <harrison.chiu.136@gmail.com>
2015-03-17 13:40:06 -0400

    did matrix things

--- matrix.rkt
+++ matrix.rkt
@@ -1,69 +1,77 @@
 #lang racket
 
-(provide
- make-matrix
- matrix-cols
+(require "contracts.rkt")
 
- add-matrix-col!
- add-matrix-cols!
-
- matrix->string
- display-matrix
-)
-
-;; MATRIX STUFF
-;; ----------------------------------------------------------------
-(struct matrix (rows (cols #:mutable)))
+(provide (contract-out
+          (make-matrix
+           (-> (listof (listof number?))
+               matrix/c))
+          (matrix-row
+           (-> matrix/c exact-nonnegative-integer?
+               (vectorof number?)))
+          (matrix-col
+           (-> matrix/c exact-nonnegative-integer?
+               (vectorof number?)))
+          (matrix-multiply
+           (-> matrix/c matrix/c
+               matrix/c))))
 
 (define make-matrix
-  (lambda (rows)
-    (matrix rows (build-list rows (lambda (n) '())))))
+  (lambda (biglist)
+    (list->vector
+     (map list->vector biglist))))
 
-(define add-matrix-col!
+(define matrix-row
+  (lambda (mtx row)
+    (vector-ref mtx row)))
+
+(define matrix-col
   (lambda (mtx col)
-    (when (= (length col) 
-             (matrix-rows mtx))
-      (set-matrix-cols! mtx
-                        (map (lambda (val row)
-                               (cons val 
-                                     row))
-                             col
-                             (matrix-cols mtx))))))
+    (vector-map
+     (curryr vector-ref col)
+     mtx)))
 
-(define add-matrix-cols!
-  (lambda (mtx . cols)
-    (when (= (length cols) 
-             (matrix-rows mtx))
-      (map (lambda (col) 
-             (add-matrix-col! mtx col)) 
-           (matrix-cols-list cols)))))
+(define matrix-rows
+  (lambda (mtx)
+    (vector-length mtx)))
+
+(define matrix-cols
+  (lambda (mtx)
+    (vector-length (matrix-row mtx 0))))
+
+(define matrix-ref
+  (lambda (mtx row col)
+    (vector-ref (matrix-row mtx row) col)))
 
 (define matrix-multiply
-  (lambda (row-mtx col-mtx)
-    (matrix-cols-list (matrix-cols col-mtx))))
+  (lambda (mtx1 mtx2)
+    (vector-map
+     (lambda (mtx-row)
+       (vector-map
+        (curry dot mtx-row)
+        (matrix-rotate mtx2)))
+     mtx1)))
 
-(define matrix-cols-list
-  (lambda (cols)
-    (eval `(map (lambda n
-                  n)
-                ,@(map (lambda (lst)
-                         `(reverse (list ,@lst))) 
-                       cols)))))
+(define dot
+  (lambda (v1 v2)
+    (foldl + 0
+           (vector->list (vector-map * v1 v2)))))
 
-(define matrix->string
+(define matrix-rotate
   (lambda (mtx)
-    (string-join
-     (map (lambda (row)
-            (string-join
-             (map (lambda (n)
-                    (number->string n))
-                  row)
-             " "))
-          (matrix-cols mtx))
-     "\n" 
-     #:after-last "\n")))
+     (vector-map (curry matrix-col mtx)
+                 (build-vector (matrix-cols mtx) identity))))
 
-(define display-matrix
-  (lambda (mtx)
-    (display (matrix->string mtx))))
-;; ================================================================
+(define ident (make-matrix '((1 0 0 0)
+                             (0 1 0 0)
+                             (0 0 1 0)
+                             (0 0 0 1))))
+(define thing (make-matrix '((1 2 3 4)
+                             (5 6 7 8)
+                             (9 10 11 12)
+                             (13 14 15 16))))
+
+;; (matrix-multiply ident thing)
+
+  
+  
