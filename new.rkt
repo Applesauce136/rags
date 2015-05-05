72fa292631dc6a735e898eac213da0dbc30bb046
Applesauce136 <harrison.chiu.136@gmail.com>
2015-05-05 13:39:44 -0400

    wrote skeleton of parser

--- new.rkt
+++ new.rkt
@@ -10,15 +10,17 @@
   (NUMBER STRING))
 
 (define-empty-tokens et
-  (PUSH POP
-        MOVE SCALE ROTATE
-        BOX SPHERE TORUS LINE
-        COMMENT SAVE
-        EOF NEWLINE))
+  (PUSH
+   POP
+   MOVE SCALE ROTATE
+   BOX SPHERE TORUS LINE
+   SAVE
+   EOF NEWLINE))
 
 (define my-lexer
   (lexer ((eof) 'EOF)
-         ((:or #\tab #\space)
+         ((:or #\tab #\space ; skip whitespace and comments 
+               (:: "#" (:* (:~ #\newline)) #\newline))
           (my-lexer input-port))
          (#\newline
           (token-NEWLINE))
@@ -27,8 +29,6 @@
                "BOX" "SPHERE" "TORUS" "LINE"
                "SAVE")
           (string->symbol lexeme))
-         ((:: "#" (:* (:~ #\newline)) #\newline)
-          (token-COMMENT))
          ((:: (:+ numeric) (:? #\.) (:* numeric))
           (token-NUMBER (string->number lexeme)))
          ((:+ (:or alphabetic numeric punctuation))
@@ -47,66 +47,26 @@
 
 ;; PARSER
 ;; ----------------------------------------------------------------
-;; (define my-parser
-;;   (parser 
-;;    (start input) 
-;;    (end EOF) 
-;;    (tokens t et) 
-;;    (grammar 
-;;     (input 
-;;      (() #f) 
-;;      ((input command) #f)) 
-;;     (command 
-;;      ((COMMENT) #f) 
-;;      ((LIGHT STRING DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE) #f) 
-;;      ((MOVE DOUBLE DOUBLE DOUBLE STRING) #f) 
-;;      ((MOVE DOUBLE DOUBLE DOUBLE) #f) 
-;;      ((CONSTANTS STRING DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE) #f) 
-;;      ((CONSTANTS STRING DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE) #f) 
-;;      ((SAVE_COORDS STRING) #f) 
-;;      ((CAMERA DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE) #f)
-;;      ((TEXTURE STRING DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE) #f) 
-;;      ((SPHERE DOUBLE DOUBLE DOUBLE DOUBLE) #f) 
-;;      ((SPHERE DOUBLE DOUBLE DOUBLE DOUBLE STRING) #f) 
-;;      ((SPHERE STRING DOUBLE DOUBLE DOUBLE DOUBLE) #f) 
-;;      ((SPHERE STRING DOUBLE DOUBLE DOUBLE DOUBLE STRING) #f) 
-;;      ((TORUS DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE) #f) 
-;;      ((TORUS DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE STRING) #f) 
-;;      ((TORUS STRING DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE) #f) 
-;;      ((TORUS STRING DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE STRING) #f) 
-;;      ((BOX DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE) #f) 
-;;      ((BOX DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE STRING) #f) 
-;;      ((BOX STRING DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE) #f) 
-;;      ((BOX STRING DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE STRING) #f) 
-;;      ((LINE DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE) #f) 
-;;      ((LINE DOUBLE DOUBLE DOUBLE STRING DOUBLE DOUBLE DOUBLE) #f) 
-;;      ((LINE DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE STRING) #f) 
-;;      ((LINE DOUBLE DOUBLE DOUBLE STRING DOUBLE DOUBLE DOUBLE STRING) #f) 
-;;      ((LINE STRING DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE) #f) 
-;;      ((LINE STRING DOUBLE DOUBLE DOUBLE STRING DOUBLE DOUBLE DOUBLE) #f) 
-;;      ((LINE STRING DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE STRING) #f) 
-;;      ((LINE STRING DOUBLE DOUBLE DOUBLE STRING DOUBLE DOUBLE DOUBLE STRING) #f) 
-;;      ((MESH CO STRING) #f) 
-;;      ((MESH STRING CO STRING) #f) 
-;;      ((MESH STRING CO STRING STRING) #f) 
-;;      ((SET STRING DOUBLE) #f) 
-;;      ((SCALE DOUBLE DOUBLE DOUBLE STRING) #f) 
-;;      ((SCALE DOUBLE DOUBLE DOUBLE) #f) 
-;;      ((ROTATE STRING DOUBLE STRING) #f) 
-;;      ((ROTATE STRING DOUBLE) #f) 
-;;      ((BASENAME STRING) #f) 
-;;      ((SAVE_KNOBS STRING) #f) 
-;;      ((TWEEN DOUBLE DOUBLE STRING STRING) #f) 
-;;      ((FRAMES DOUBLE) #f) 
-;;      ((VARY STRING DOUBLE DOUBLE DOUBLE DOUBLE) #f) 
-;;      ((PUSH) #f) 
-;;      ((GENERATE_RAYFILES) #f) 
-;;      ((POP) #f) 
-;;      ((SAVE STRING) #f) 
-;;      ((SHADING SHADING_TYPE) #f) 
-;;      ((SETKNOBS DOUBLE) #f) 
-;;      ((FOCAL DOUBLE) #f) 
-;;      ((DISPLAY) #f) 
-;;      ((WEB) #f) 
-;;      ((AMBIENT DOUBLE DOUBLE DOUBLE) #f)))))
-;; ;; ================================================================
+(define my-parser
+  (parser 
+   (start input) 
+   (end NEWLINE EOF) 
+   (tokens t et)
+   (error (lambda (a b c) (void)))
+   
+   (grammar 
+    (input 
+     (() #f) 
+     ((input command) #f)) 
+    (command 
+     ((PUSH) #f)
+     ((POP) #f)
+     ((MOVE NUMBER NUMBER NUMBER) #f)
+     ((SCALE NUMBER NUMBER NUMBER) #f)
+     ((ROTATE NUMBER NUMBER NUMBER) #f)
+     ((BOX NUMBER NUMBER NUMBER) #f)
+     ((SPHERE NUMBER) #f)
+     ((TORUS NUMBER) #f)
+     ((LINE NUMBER NUMBER NUMBER) #f)
+     ((SAVE STRING) #f)))))
+;; ================================================================
