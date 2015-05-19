9e70453277b84ff45cae113efe1225eae4feeb8e
Applesauce136 <harrison.chiu.136@gmail.com>
2015-05-19 09:11:36 -0400

    some changes of some kind

--- compiler.rkt
+++ compiler.rkt
@@ -2,7 +2,13 @@
 
 (require parser-tools/lex
          parser-tools/yacc
+<<<<<<< Updated upstream
          (prefix-in : parser-tools/lex-sre))
+=======
+         (prefix-in : parser-tools/lex-sre)
+         
+         racket/generator)
+>>>>>>> Stashed changes
 
 (provide get-commands)
 
@@ -12,12 +18,22 @@
   (NUMBER STRING))
 
 (define-empty-tokens et
+<<<<<<< Updated upstream
   (FRAMES VARY BASENAME
           PUSH POP
           MOVE SCALE ROTATE
           BOX SPHERE TORUS LINE
           SAVE
           EOF NEWLINE))
+=======
+  (VARY
+   BASENAME FRAMES
+   PUSH POP
+   MOVE SCALE ROTATE
+   BOX SPHERE TORUS LINE
+   SAVE
+   EOF NEWLINE))
+>>>>>>> Stashed changes
 
 (define my-lexer
   (lexer ((eof) 'EOF)
@@ -26,12 +42,20 @@
           (my-lexer input-port))
          (#\newline
           (token-NEWLINE))
+<<<<<<< Updated upstream
          ((:or "FRAMES" "VARY" "BASENAME"
+=======
+         ((:or "VARY" "BASENAME" "FRAMES"
+>>>>>>> Stashed changes
                "PUSH" "POP"
                "MOVE" "SCALE" "ROTATE"
                "BOX" "SPHERE" "TORUS" "LINE"
                "SAVE"
+<<<<<<< Updated upstream
                "frames" "vary" "basename"
+=======
+               "vary" "basename" "frames"
+>>>>>>> Stashed changes
                "push" "pop"
                "move" "scale" "rotate"
                "box" "sphere" "torus" "line"
@@ -65,16 +89,32 @@
     
     (command
      ((FRAMES NUMBER)
+<<<<<<< Updated upstream
       `(frames ,$2))
      ((BASENAME STRING)
       `(basename ,$2))
      ((VARY STRING NUMBER NUMBER NUMBER NUMBER)
       `(vary (string->symbol ,$2) ,$3 ,$4 ,$5 ,$6))
+=======
+      `(set! varys (make-vector ,$2 make-hasheq)))
+     ((BASENAME STRING)
+      `(set! basename ,$2))
+     ((VARY STRING NUMBER NUMBER NUMBER NUMBER)
+      `(do ((index ,$3 (+ index 1))
+            (value ,$5 (+ value (* (/ (- index ,$3)
+                                      (- ,$4 ,$3))
+                                   ,$6))))
+           ((= index ,$4) (hash-set! (vector-ref varys index)
+                                     (string->symbol ,$2) value))
+         (hash-set! (vector-ref varys index)
+                    (string->symbol ,$2) value)))
+>>>>>>> Stashed changes
      ((PUSH)
       '(push))
      ((POP)
       '(pop))
      ((MOVE NUMBER NUMBER NUMBER)
+<<<<<<< Updated upstream
       `(move ,$2 ,$3 ,$4))
      ((MOVE NUMBER NUMBER NUMBER STRING)
       `(move ,$2 ,$3 ,$4 ,$5))
@@ -86,6 +126,33 @@
      `(rotate (string->symbol ,$2) ,$3))
      ((ROTATE STRING NUMBER STRING)
      `(rotate (string->symbol ,$2) ,$3 (string->symbol ,$4)))
+=======
+      `(set! stack (cons (compose (move ,$2 ,$3 ,$4)
+                                  (first stack))
+                         (rest stack))))
+     ((MOVE NUMBER NUMBER NUMBER STRING)
+      `(set! stack (cons (compose (apply move (map (curry * (hash-ref current-varys (string->symbol ,$5)))
+                                                   (,$2 ,$3 ,$4)))
+                                  (first stack))
+                         (rest stack))))
+     ((SCALE NUMBER NUMBER NUMBER)
+      `(set! stack (cons (compose (scale ,$2 ,$3 ,$4)
+                                  (first stack))
+                         (rest stack))))
+     ((SCALE NUMBER NUMBER NUMBER STRING)
+      `(set! stack (cons (compose (apply move (map (curry * (hash-ref current-varys (string->symbol ,$5)))
+                                                   (,$2 ,$3 ,$4)))
+                                  (first stack))
+                         (rest stack))))
+     ((ROTATE STRING NUMBER)
+      `(set! stack (cons (compose (rotate (string->symbol ,$2) ,$3)
+                                  (first stack))
+                         (rest stack))))
+     ((ROTATE STRING NUMBER STRING)
+      `(set! stack (cons (compose (rotate (string->symbol ,$2) (* (hash-ref current-varys (string->symbol ,$4)) ,$3))
+                                  (first stack))
+                         (rest stack))))
+>>>>>>> Stashed changes
      ((BOX NUMBER NUMBER NUMBER
            NUMBER NUMBER NUMBER)
       `(box ,$2 ,$3 ,$4 ,$5 ,$6 ,$7))
@@ -106,10 +173,15 @@
 ;; ----------------------------------------------------------------
 (define get-commands
   (lambda (in)
-    (let ((result (my-parser
-                   (lambda ()
-                     (my-lexer in)))))
-      (if result
-          (cons result (get-commands in))
-          '()))))
+    (generator
+     ()
+     (let one-match ((result (my-parser
+                              (lambda ()
+                                (my-lexer in)))))
+       (if result
+           (begin (yield result)
+                  (one-match (my-parser
+                              (lambda ()
+                                (my-lexer in)))))
+           #f)))))
 ;; ================================================================
--- main.rkt
+++ main.rkt
@@ -4,16 +4,27 @@
          "commands.rkt"
          "compiler.rkt")
 
+<<<<<<< Updated upstream
 ;; BITMAP
 ;; ----------------------------------------------------------------
 ;; (define width 500)
 ;; (define height 500)
 (define my-bitmap-dc (new bitmap-dc% (bitmap
                                       (make-bitmap 500 500))))
+=======
+(define basename "")
+(define varys (vector))
+(define stack '())
+(set! stack (cons identity stack))
+(define width 500)
+(define height 500)
+(define my-bitmap-dc (new bitmap-dc% (bitmap (make-bitmap width height))))
+>>>>>>> Stashed changes
 (send my-bitmap-dc set-background (make-color 0 0 0))
 (send my-bitmap-dc clear)
 (define draw-pixels
   (lambda (gen)
+<<<<<<< Updated upstream
     (call-with-values (lambda ()
                         (send my-bitmap-dc get-size))
       (lambda (width height)
@@ -37,6 +48,21 @@
     (send (send my-bitmap-dc get-bitmap)
           save-file filename 'png)))
 ;; ================================================================
+=======
+    (let draw-pixel
+        ((val (gen)))
+      (if val
+          (begin
+            (call-with-values (lambda () (apply values (map exact-floor val)))
+              (lambda (x y z)
+                ;; (printf "x: ~a    y: ~a~n" x y)
+                (when (and (< -1 x width)
+                           (< -1 y height))
+                  (send my-bitmap-dc set-pixel
+                        x y (make-color 255 255 0)))))
+            (draw-pixel (gen)))
+          #f))))
+>>>>>>> Stashed changes
 
 ;; ANIMATION
 ;; ----------------------------------------------------------------
@@ -115,6 +141,7 @@
 (when (terminal-port? (current-input-port))
   (display "Enter the name of your file: "))
 
+<<<<<<< Updated upstream
 (define-namespace-anchor commands-ns-anchor)
 (define ns (namespace-anchor->namespace commands-ns-anchor))
 
@@ -123,3 +150,18 @@
        (call-with-input-file (symbol->string (read))
          get-commands)))
 ;; ================================================================
+=======
+(printf "Working...~n")
+
+(define-namespace-anchor commands-ns-anchor)
+(define ns (namespace-anchor->namespace
+            commands-ns-anchor))
+(call-with-input-file (symbol->string (read))
+  (lambda (in)
+    (define commands (get-commands in))
+    (let one-command ((command (commands)))
+      (if command
+          (begin (eval command ns)
+                 (one-command (commands)))
+          (printf "Done!~n")))))
+>>>>>>> Stashed changes
